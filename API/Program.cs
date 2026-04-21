9b5523b3eea9404ce9448df27ff319037aa3b530
Matias <matiasprofe2021@gmail.com>
2026-04-21 16:09:03 -0300

    agrego logica de frontend

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("frontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -8,6 +18,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("frontend");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
